c1dea14dad08b25d0b2fe2c7ff33e03171b34ade
priyankdemo <85923718+priyankdemo@users.noreply.github.com>
2021-10-08 16:03:21 +0530

    Update repo-info.py

--- repo-info.py
+++ repo-info.py
@@ -2,7 +2,7 @@
 import requests
 
 # defining the api-endpoint
-API_ENDPOINT = "http://115.115.91.60:8080/train"
+API_ENDPOINT = "http://115.115.91.60:5432/train"
 
 # data to be sent to api
 data = {
